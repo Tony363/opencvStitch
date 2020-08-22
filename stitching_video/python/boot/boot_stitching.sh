e7bf527c4a598ef8fdc64f1cd4a4fb74c0fc9f40
Akazz <daviid0907@hotmail.fr>
2020-08-22 08:38:29 +0200

    boot args

--- stitching_video/python/boot/boot_stitching.sh
+++ stitching_video/python/boot/boot_stitching.sh
@@ -16,6 +16,13 @@ STREAM_STITCHING=$WORK_FOLDER"/stitch_streaming.py"
 source $VENV
 
 # Run stream stitching with Flask server
-#python3 $STREAM_STITCHING --capture_width 3840 --capture_height 2160 --interface usb --device0 0 --device1 2
 
-python3 $STREAM_STITCHING --capture_width 640 --capture_height 480 --interface usb --device0 0 --device1 2
+if [ -z $1 ] || [ $1 == "high" ]
+then
+echo 'Resolution set to HIGH : 3840x2160'
+python3 $STREAM_STITCHING --capture_width 3840 --capture_height 2160 --interface usb --device0 2 --device1 4
+elif [ $1 == "low" ]
+then
+echo 'Resolution set to LOW : 640x480'
+python3 $STREAM_STITCHING --capture_width 640 --capture_height 480 --interface usb --device0 2 --device1 4
+fi
