7b962e8547fe3afcbd456ebc19ec4aba0cb2ec36
tony363 <pysolver33@gmail.com>
2020-10-01 18:49:06 +0800

    barebones image rotation, need to implement it by gpu

--- stitching_video/python/matrixTransform.py
+++ stitching_video/python/matrixTransform.py
@@ -5,16 +5,22 @@ from utils import timer
 img = cv.resize(cv.imread("inputs/left.png"),(640,480))
 theta = np.radians(30)
 c,s = np.cos(theta),np.sin(theta)
-rmatrix = np.asarray([(c,-s,0),(s,c,0),(0,0,1)])
-print(rmatrix)
-print(img.shape)
-
+rmatrix = np.asarray(
+    [
+    (c,s,0),
+    (-s,c,0),
+    (0,0,1)
+        ])
+T_s = np.array([[2, 0, 0], [0, 2, 0], [0, 0, 1]])
+T_r = np.array([[0, 1, 0], [-1, 0, 0], [0, 0, 1]])
+T = T_s @ T_r
+img_transform = np.empty((480*2,640*2,3),dtype=np.uint8)
 for i,row in enumerate(img):
     for j,col in enumerate(row):
         pixel_data = img[i,j,:]
         input_coords = np.array([i,j,1])
-        i_out,j_out,_ = rmatrix * input_coords
+        i_out,j_out,_ =  rmatrix @ input_coords
         start_time = timer(start_time=None)
-        img[i_out.astype(np.int64),j_out.astype(np.int64),:] = pixel_data
+        img_transform[i_out.astype(np.int64),j_out.astype(np.int64),:] = pixel_data
         print(timer(start_time=start_time))
-cv.imwrite("outputs/rotate.png",img)
+cv.imwrite("outputs/rotate.png",img_transform)
